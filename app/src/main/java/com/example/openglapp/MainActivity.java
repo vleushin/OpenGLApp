e4330ace205570c8cf08cea1a8ca33311772dc70
vleushin <vleushin@gmail.com>
2018-02-20 22:31:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/openglapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.openglapp;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private GLSurfaceView mGLView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+}
---
+++ app/src/main/java/com/example/openglapp/MyGLSurfaceView.java
@@ -0,0 +1,23 @@
+package com.example.openglapp;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(mRenderer);
+
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+}
